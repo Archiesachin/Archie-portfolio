d99bd898a0d6e7cecd16a692597064114beb2ca5
Archiesachin <archieshah8767@gmail.com>
2024-05-09 19:02:34 +0530

    contact form

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
           </div>
 
           <div className='w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8 py-8 rounded-r-[10px]'>
-            <form action="contact.php"className='w-full '>
+            <form action=""className='w-full '>
               <div className='mb-5'>
                 <input type="text" placeholder='Enter your name' className='w-full p-3 focus:outline-none rounded-[8px]' />
               </div>
